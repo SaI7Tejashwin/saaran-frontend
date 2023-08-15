b2f8fcccbb8b71b6dcb2e5151f4480e19fa30428
Lakshin Kumar <lakshin.kumar.lk@gmail.com>
2023-08-15 23:37:45 +0530

    removed boilerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  //templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
   template: `<h1>Hello world!</h1>`,
 })
 export class AppComponent {
