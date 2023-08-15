87e1161b8057e9450b13b1ac708eb9aa4ef3c741
Lakshin Kumar <lakshin.kumar.lk@gmail.com>
2023-08-15 19:56:23 +0530

    Hello World! added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //templateUrl: './app.component.html',
+  //styleUrls: ['./app.component.css'],
+  template: `<h1>Hello world!</h1>`,
 })
 export class AppComponent {
-  title = 'summarization-app-frontend';
+  title = 'saaran';
 }
