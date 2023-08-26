3f724259cb456e2fc6d71b9785c240b2ffeacaef
Lakshin Kumar <lakshin.kumar.lk@gmail.com>
2023-08-26 17:55:34 +0530

    working boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { NavbarComponent } from 'src/app/navbar/navbar.component';
-import { SideNavbarComponent } from 'src/app/sidenavbar/sidenavar.component';
+import { SidenavbarComponent } from 'src/app/sidenavbar/sidenavbar.component';
 import { UploadComponent } from 'src/app/upload/upload.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    SideNavbarComponent,
-    UploadComponent
   ],
   imports: [
     BrowserModule,
